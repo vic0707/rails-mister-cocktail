db61414977a5e07ea04b05cdb8ce6e262375d153
Victor Macias <v.maciascastillo@gmail.com>
2016-11-24 11:34:09 +0100

    model complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "white sugar")
+Ingredient.create(name: "cinnamon")
+Ingredient.create(name: "brown sugar")
