570ab7ae435c389ac52b5cb633c72e309a43515f
Victor Macias <v.maciascastillo@gmail.com>
2016-11-27 18:16:55 +0100

    grid

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
+gem 'materialize-sass'
 
 #Image upload gems
 gem 'figaro'
--- app/uploaders/cocktail_photo_uploader.rb
+++ app/uploaders/cocktail_photo_uploader.rb
@@ -7,8 +7,8 @@ class CocktailPhotoUploader < CarrierWave::Uploader::Base
   process convert: 'jpg'
   
   version :bright_face do
-    cloudinary_transformation effect: "brightness:30", radius: 20,
-    width: 150, height: 150, crop: :thumb, gravity: :face
+    cloudinary_transformation effect: "brightness:10", radius: 20,
+    width: 150, height: 150, crop: :thumb
   end
 
 end
